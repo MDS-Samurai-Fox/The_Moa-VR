bef972c8d4a4756c1f4130c0685051755349c6fd
Juan Rodriguez <timrodz@icloud.com>
2017-05-29 17:34:05 +1200

    Add tree with hover text

---
+++ Assets/Scripts/TeleportAreaUnlocker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TeleportAreaUnlocker : MonoBehaviour {
+
+	public TeleportArea teleportArea;
+
+	public void Unlock() {
+		teleportArea.locked = false;
+	}
+
+	public void Lock() {
+		teleportArea.locked = true;
+	}
+
+}
---
+++ Assets/Scripts/TeleportPointUnlocker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class TeleportPointUnlocker : MonoBehaviour {
+
+	public TeleportArea teleportPoint;
+
+	public void Unlock() {
+		teleportPoint.locked = false;
+	}
+
+	public void Lock() {
+		teleportPoint.locked = true;
+	}
+
+}
