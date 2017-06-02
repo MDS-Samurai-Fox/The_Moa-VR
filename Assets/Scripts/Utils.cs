857fb81f0242484d95ade4c890021c94ecbcaecf
Juan Rodriguez <timrodz@icloud.com>
2017-06-02 13:02:40 +1200

    Add utils and canvas animator

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Utils : MonoBehaviour {
+
+    public static Utils Instance { get; private set; }
+
+	private float fadeDuration = 1;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake () {
+
+        if (Instance != null && Instance != this) {
+            Destroy (gameObject);
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad (gameObject);
+
+    }
+
+    public void FadeIn (CanvasGroup cg) {
+
+        cg.DOFade (1, fadeDuration);
+
+    }
+
+    public void FadeOut (CanvasGroup cg) {
+
+        cg.DOFade (0, fadeDuration);
+
+    }
+
+	public void SetFadeDuration(float fadeDuration) {
+
+		this.fadeDuration = fadeDuration;
+
+	}
+
+}
